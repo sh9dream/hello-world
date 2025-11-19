a7255008cc6c89b85b2e3f68cff61ede2361ac10
sh9dream <30896093+sh9dream@users.noreply.github.com>
2025-11-19 17:05:35 +0545

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -7,8 +7,9 @@ import os
 # Load environment variables from .env
 load_dotenv()
 # Fetch variables
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.environ.get("https://dqxobamrnoeccpuxekyh.supabase.co")
+key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxeG9iYW1ybm9lY2NwdXhla3loIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDQ3ODEyNSwiZXhwIjoyMDc2MDU0MTI1fQ.D9-_AbzNZJe_9X5mqGRPfmdChX961FHDxgE67IL84do")
 
 # Connect to the database
 supabase: Client = create_client(url, key)
+
